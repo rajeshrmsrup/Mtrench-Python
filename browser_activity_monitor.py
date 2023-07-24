1144fbc5b6ad8f672b26ed2c0af5e1c11888772d
Mr Rajesh <34417363+rajeshrmsrup@users.noreply.github.com>
2023-07-24 21:31:21 +0530

    Update browser_activity_monitor.py

--- browser_activity_monitor.py
+++ browser_activity_monitor.py
@@ -1,6 +1,13 @@
 import time
 import psutil
 import pygetwindow
+import ctypes
+
+# Windows API constants
+GWL_HWNDPARENT = -8
+WM_GETTEXTLENGTH = 0x000E
+WM_GETTEXT = 0x000D
+BUFFER_SIZE = 255
 
 def get_active_browser_pid():
     for process in psutil.process_iter(['pid', 'name']):
@@ -8,31 +15,50 @@ def get_active_browser_pid():
             return process.info['pid']
     return None
 
-def get_active_tab_title(pid):
+def get_active_tab_title_and_url(pid):
     try:
         browser = pygetwindow.getWindowsWithTitle('Google Chrome')[0]
         if browser.isActive:
-            return browser.title
-        return None
+            active_tab_title = browser.title
+            active_tab_url = get_url_from_window_handle(browser._hWnd)
+            return active_tab_title, active_tab_url
+        return None, None
     except IndexError:
-        return None
+        return None, None
+
+def get_url_from_window_handle(hwnd):
+    buffer = ctypes.create_unicode_buffer(BUFFER_SIZE)
+    hwnd_parent = ctypes.windll.user32.GetParent(hwnd)
+    url_length = ctypes.windll.user32.SendMessageW(hwnd_parent, WM_GETTEXTLENGTH, 0, 0)
+    ctypes.windll.user32.SendMessageW(hwnd_parent, WM_GETTEXT, url_length + 1, buffer)
+    return buffer.value
 
 def monitor_activity(log_file):
+    active_site_start_time = None
     while True:
         active_browser_pid = get_active_browser_pid()
 
         if active_browser_pid is not None:
             timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-            active_tab_title = get_active_tab_title(active_browser_pid)
+            active_tab_title, active_tab_url = get_active_tab_title_and_url(active_browser_pid)
 
             if active_tab_title:
+                if active_site_start_time is None:
+                    active_site_start_time = time.time()
+
                 with open(log_file, 'a') as f:
                     f.write(f"Timestamp: {timestamp}\n")
-                    f.write(f"Website URL: N/A (Not available for this implementation)\n")
+                    f.write(f"Website URL: {active_tab_url}\n")
                     f.write(f"Page Title: {active_tab_title}\n")
-                    # Additional implementation is needed to get the URL and total time duration.
-                    f.write("Total time duration users stay on each site: N/A (Not available for this implementation)\n")
+                    
+                # Calculate the total time duration users stay on the site
+                active_site_duration = time.time() - active_site_start_time
+                with open(log_file, 'a') as f:
+                    f.write(f"Total time duration users stay on each site: {active_site_duration:.2f} seconds\n")
                     f.write("\n")
+
+        else:
+            active_site_start_time = None
 
         time.sleep(5)  # Check every 5 seconds
 
